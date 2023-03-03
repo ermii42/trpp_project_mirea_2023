f1a89a41aa23406fe2a2f217eca2004eb292f9c0
Ermishova SM <svetlana.ermishova1@yandex.ru>
2023-03-03 16:53:34 +0300

    adding spring

---
+++ src/main/java/com/example/noteswebapp/NotesWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWebAppApplication.class, args);
+	}
+
+}
